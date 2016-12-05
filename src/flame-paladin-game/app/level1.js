98111157005f5e5c98327226e675d321082ea6b1
Thomas N Rizzo III <thomasnrizzo@gmail.com>
2016-12-05 15:13:00 -0700

    added keyboard functionality to menu screen.

---
+++ src/flame-paladin-game/app/level1.js
@@ -0,0 +1,13 @@
+Game.Level1 = function(game){}
+
+Game.Level1.prototype = {
+
+  create: function (game) {
+
+  },
+
+  update: function (game) {
+
+  },
+
+}
--- src/flame-paladin-game/app/menu.js
+++ src/flame-paladin-game/app/menu.js
@@ -1,8 +1,10 @@
 Game.Menu= function(game){}
 
+var upKey;
 var timer = 0;
 var text;
 var text2;
+
 
 Game.Menu.prototype = {
 
@@ -18,7 +20,9 @@ Game.Menu.prototype = {
     text = game.add.text(0, 0, "The Last Flame Paladin II", style);
     text.setTextBounds(0, 100, 800, 100);
 
-    text2 = game.add.text(225, 500, "PRESS START", style);
+    text2 = game.add.text(225, 500, "PRESS ENTER", style);
+
+    upKey = game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
 
     },
 
@@ -26,11 +30,15 @@ Game.Menu.prototype = {
   update: function(game) {
 
     timer += game.time.elapsed;
-      if (timer >= 500){
-          timer -= 500;
-          text2.visible = !text2.visible;
+      if (timer >= 500) {
+        timer -= 500;
+        text2.visible = !text2.visible;
       }
 
-  },
+      if (upKey.isDown) {
+          this.state.start('Level1', true, false);
+      }
+
+    },
 
 };
