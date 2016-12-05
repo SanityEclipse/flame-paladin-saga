5c2b10cbc1596b330d1d313fffe8fa5438344c93
Thomas N Rizzo III <thomasnrizzo@gmail.com>
2016-12-05 14:06:13 -0700

    additional styling added to main menu page.

--- src/flame-paladin-game/app/menu.js
+++ src/flame-paladin-game/app/menu.js
@@ -1,19 +1,35 @@
-Game.Menu= function(game){
+Game.Menu= function(game){}
 
-}
+var timer = 0;
+var text;
+var text2;
 
 Game.Menu.prototype = {
 
   create: function(game){
-        var style = {
-          font: "30px Press Start 2P", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" };
-          text = game.add.text(0, 0, "The Last Flame Paladin II", style);
-          text.setShadow(3, 3, 'rgba(0,0,0,0.5)', 2);
-          text.setTextBounds(0, 100, 800, 100);
 
-  },
+    backgroundMusic = game.add.audio('menu');
+    backgroundMusic.loop = true;
+    backgroundMusic.play();
+
+    var style = {
+      font: "28px Press Start 2P", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" };
+
+    text = game.add.text(0, 0, "The Last Flame Paladin II", style);
+    text.setTextBounds(0, 100, 800, 100);
+
+    text2 = game.add.text(225, 500, "PRESS START", style);
+
+    },
+
 
   update: function(game) {
+
+    timer += game.time.elapsed;
+      if (timer >= 500){
+          timer -= 500;
+          text2.visible = !text2.visible;
+      }
 
   },
 
