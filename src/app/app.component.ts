5c1d7ff730eba438cc6562c7471acdb47473d887
Thomas N Rizzo III <thomasnrizzo@gmail.com>
2016-12-07 16:05:20 -0700

    Basic templating for landing page complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 
+
 @Component({
+
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+
 })
 export class AppComponent {
   title = 'The Flame Paladin Saga';
