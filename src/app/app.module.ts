5de950b669124ed4ec8e90e8635237447c85ec83
Thomas N Rizzo III <thomasnrizzo@gmail.com>
2016-12-07 13:10:01 -0700

    Pre branch commit. Added next module for site landing.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { GameCanvasComponent } from './game-canvas/game-canvas.component';
+import { SiteLandingComponent } from './site-landing/site-landing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GameCanvasComponent
+    GameCanvasComponent,
+    SiteLandingComponent
   ],
   imports: [
     BrowserModule,
