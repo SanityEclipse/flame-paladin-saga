0f57118a4c2c9659d30562355b452dd425d7b326
Thomas N Rizzo III <thomasnrizzo@gmail.com>
2016-12-08 12:35:16 -0700

    Phaser game added to appropriate module, and is rendering properly

--- src/app/game-canvas/game-canvas.component.ts
+++ src/app/game-canvas/game-canvas.component.ts
@@ -10,6 +10,19 @@ export class GameCanvasComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-  }
 
+        window.onload = function() {
+          var game = new Phaser.Game(
+            800, 600,
+            Phaser.AUTO,
+            'game-renders-here');
+          game.state.add('Boot', Game.Boot);
+          game.state.add('Preloader', Game.Preloader);
+          game.state.add('Menu', Game.Menu);
+          game.state.add('Level1', Game.Level1);
+            // game.state.add('GameOver', Game.GameOver);
+          game.state.start('Boot');
+
+    }
+  }
 }
--- src/app/game-landing/game-landing.component.ts
+++ src/app/game-landing/game-landing.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
-
 @Component({
   selector: 'app-game-landing',
   templateUrl: './game-landing.component.html',
