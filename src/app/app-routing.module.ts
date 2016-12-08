e199a17636a00e7dfe911bb309883ff8cc3f7bb3
Thomas N Rizzo III <thomasnrizzo@gmail.com>
2016-12-07 18:36:03 -0700

    Contact page is finished.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { GameCanvasComponent } from './game-canvas/game-canvas.component';
-import { SiteLandingComponent} from './site-landing/site-landing.component';
+import { GameCanvasComponent }  from './game-canvas/game-canvas.component';
+import { SiteLandingComponent } from './site-landing/site-landing.component';
+import { ContactInfoComponent } from './contact-info/contact-info.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/site-landing', pathMatch: 'full'},
   { path: 'game-canvas', component: GameCanvasComponent },
-  { path: 'site-landing', component: SiteLandingComponent }
+  { path: 'site-landing', component: SiteLandingComponent },
+  { path: 'contact-info', component: ContactInfoComponent }
 ];
 
 @NgModule({
