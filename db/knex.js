66ed71c72c2476e89a472f3313aa2cdb88ee3e1b
Thomas N Rizzo III <thomasnrizzo@gmail.com>
2016-12-14 12:26:00 -0700

    bug fix

--- db/knex.js
+++ db/knex.js
@@ -1,10 +1,9 @@
 "use strict"
 
 require('dotenv').config();
-
+const environment = process.env.NODE_ENV || 'development';
 const knex = require('knex');
 const config = require('../knexfile')[environment];
-const environment = process.env.NODE_ENV || 'development';
 
 let pg = knex(config[environment]);
 
