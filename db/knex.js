9ba505b98e2118ada529ef763e89651108846593
Thomas N Rizzo III <thomasnrizzo@gmail.com>
2016-12-14 12:20:33 -0700

    nug fix

--- db/knex.js
+++ db/knex.js
@@ -6,6 +6,6 @@ const knex = require('knex');
 const config = require('../knexfile')[environment];
 const environment = process.env.NODE_ENV || 'development';
 
-let pg = knex(config[env]);
+let pg = knex(config[environment]);
 
 module.exports = pg
