b15c9d01618db9a1e040af2c41ebdcf9ab5ad7c6
Thomas N Rizzo III <thomasnrizzo@gmail.com>
2016-12-13 23:24:19 -0700

    Canvas is being destroyed properly, but is not re-rendering in the correct spot.

--- src/app/game-canvas/game-canvas.component.ts
+++ src/app/game-canvas/game-canvas.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-game-canvas',
@@ -12,27 +12,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-canvas.component.css']
 })
 
-export class GameCanvasComponent implements OnInit {
+export class GameCanvasComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  constructor() {
+    this.game = new Phaser.Game(725, 525, Phaser.AUTO,'game-renders-here');
 
+    game: Phaser.Game;
+
+    this.game.state.add('Boot', Game.Boot);
+    this.game.state.add('Preloader', Game.Preloader);
+    this.game.state.add('Menu', Game.Menu);
+    this.game.state.add('Directions', Game.Directions);
+    this.game.state.add('Level1', Game.Level1);
+    this.game.state.add('Endgame', Game.Endgame);
+
+}
   ngOnInit() {
+    this.game.state.start('Boot');
+  }
 
-            // window.onload = function() {
-          var game = new Phaser.Game(
-            725, 525,
-            Phaser.AUTO,
-            'game-renders-here');
-
-          game.state.add('Boot', Game.Boot);
-          game.state.add('Preloader', Game.Preloader);
-          game.state.add('Menu', Game.Menu);
-          game.state.add('Directions', Game.Directions);
-          game.state.add('Level1', Game.Level1);
-          game.state.add('Endgame', Game.Endgame);
-
-          game.state.start('Boot');
-
-    // }
+  ngOnDestroy() {
+    this.game.destroy();
+    console.log("ngOnDestroy");
   }
 }
