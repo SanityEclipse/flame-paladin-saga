ec6de69abf71dbcc1357d0a5387622c99328cae3
Thomas N Rizzo III <thomasnrizzo@gmail.com>
2016-12-21 18:58:52 -0700

    Pre-merge commit

--- dist/assets/flame-paladin-game/app/boot.js
+++ dist/assets/flame-paladin-game/app/boot.js
@@ -2,6 +2,8 @@ var Game = {};
 
 Game.Boot = function(game) {};
 
+var scaleRatio = window.devicePixelRatio / 3;
+
 Game.Boot.prototype = {
     init: function() {
         this.input.maxpointers = 1;
@@ -12,6 +14,18 @@ Game.Boot.prototype = {
         this.load.image('preloaderBar', 'assets/flame-paladin-game/assets/images/world/load-bar.png')
     },
     create: function() {
-        this.state.start('Preloader')
+
+    // this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    //
+    // this.scale.pageAlignHorizontally = true;
+    //
+    // this.scale.pageAlignVertically = true;
+    //
+    // this.scale.setScreenSize(true);
+    //
+    // this.game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    this.state.start('Preloader')
+
     }
 }
--- src/assets/flame-paladin-game/app/boot.js
+++ src/assets/flame-paladin-game/app/boot.js
@@ -2,6 +2,8 @@ var Game = {};
 
 Game.Boot = function(game) {};
 
+var scaleRatio = window.devicePixelRatio / 3;
+
 Game.Boot.prototype = {
     init: function() {
         this.input.maxpointers = 1;
@@ -9,9 +11,13 @@ Game.Boot.prototype = {
     },
 
     preload: function() {
+      
         this.load.image('preloaderBar', 'assets/flame-paladin-game/assets/images/world/load-bar.png')
     },
+
     create: function() {
+
         this.state.start('Preloader')
+
     }
 }
